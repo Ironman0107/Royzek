8486e103d26309bf201205706abe1eb869162b06
Ironman0107 <34966015+Ironman0107@users.noreply.github.com>
2017-12-30 21:06:53 +0300

    C# Video Çekme
    Video Çekme

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Masaüstünden_resım_video_cekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\Mert\\Desktop\\2.mp3"; // media playere yol göstererek masaüstündeki müziği açıyoruz.
+            pictureBox1.ImageLocation = "C:\\Users\\Mert\\Desktop\\aas.jpg";
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\Mert\\Desktop\\1.mp3"; // media playere yol göstererek masaüstündeki müziği açıyoruz.
+            pictureBox1.ImageLocation = "C:\\Users\\Mert\\Desktop\\as.jpg";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\Mert\\Desktop\\can\\bg.mp3"; // media playere yol göstererek masaüstündeki müziği açıyoruz.
+            pictureBox1.ImageLocation = "C:\\Users\\Mert\\Desktop\\as.jpg";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            axWindowsMediaPlayer1.close();
+            
+        }
+    }
+}
